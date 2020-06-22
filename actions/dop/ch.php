623adff1ead3553728927e3f7a059faafdc6bdd9
mia-im <62039441+mia-im@users.noreply.github.com>
2020-06-22 23:57:05 +0300

    Update ch.php

--- actions/dop/ch.php
+++ actions/dop/ch.php
@@ -1,17 +1,29 @@
 <?php
 session_start();
 $link = mysqli_connect('localhost', 'root' , '','lab');
- 
-$id=$_GET['id'];  
-	mysqli_query($link, "UPDATE `users` SET `name` = '{$_POST['name']}',`surname` = '{$_POST['surname']}',`login` = '{$_POST['login']}', `password` = '{$_POST['password']}',`lang` = '{$_POST['lang']}' WHERE `id`='" . mysqli_real_escape_string($link, $id) . "';");
+
+$id=(int)$_GET['id'];
 
 if ($id==$_SESSION['id']) {
-	$result = mysqli_query($link, "SELECT * FROM `users` WHERE `id` = '$id'");
+	$stmt = mysqli_prepare($link, "UPDATE `users` SET `name` = ?,`surname` = ?,`login` = ?, `password` = ?,`lang` = ? WHERE `id` = ?");
+mysqli_stmt_bind_param($stmt, "sssssi", $_POST['name'], $_POST['surname'], $_POST['login'], $_POST['password'], $_POST['lang'], $_GET['id']);
+mysqli_stmt_execute($stmt);
+
+	$stmt = mysqli_prepare($link, "SELECT * FROM `users` WHERE `id` = ?");
+mysqli_stmt_bind_param($stmt, "i", $_GET['id']);
+
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+	
 	include 'session.php';
 	header("Location: ../../users/".strtolower($_SESSION['role']).".php");
 }
 else{
-	mysqli_query($link, "UPDATE `users` SET `role` = '{$_POST['role']}' WHERE `id`='" . mysqli_real_escape_string($link, $id) . "';");
+	$stmt = mysqli_prepare($link, "UPDATE `users` SET `name` = ?,`surname` = ?,`login` = ?, `password` = ?,`lang` = ?,`role` = ? WHERE `id` = ?");
+mysqli_stmt_bind_param($stmt, "sssssii", $_POST['name'], $_POST['surname'], $_POST['login'], $_POST['password'], $_POST['lang'], $_POST['role'], $_GET['id']);
+
+mysqli_stmt_execute($stmt);
+
 	header("Location:../list.php");
 }
 ?>
