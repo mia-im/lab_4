5ea906f5d3fa9e8ac523ca6c933635bb50e38de4
mia-im <62039441+mia-im@users.noreply.github.com>
2020-06-22 23:59:54 +0300

    Update users_buttons.php

--- actions/dop/users_buttons.php
+++ actions/dop/users_buttons.php
@@ -1,4 +1,3 @@
 <?php include '../lang/language.php';?>
-<br><input type="button" value="<?php echo lang::trans('Users List'); ?>" onclick="location.href='../actions/list.php'"><br>
-<br><input type="button" value="<?php echo lang::trans('Settings'); ?>" onclick="location.href='../actions/settings.php'"><br>
-<br><input type="button" value="<?php echo lang::trans('Log out'); ?>" onclick="location.href='../login/main.php'">
+<br><a href="../actions/list.php" > <?php echo lang::trans('Users List'); ?></a><br>
+<br><a href="../login/main.php" > <?php echo lang::trans('Log out'); ?></a>
