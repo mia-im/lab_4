f9213c54aea36d07ad79ceae4ade5716d57f59d6
mia-im <62039441+mia-im@users.noreply.github.com>
2020-06-22 23:59:08 +0300

    Update reg.php

--- actions/dop/reg.php
+++ actions/dop/reg.php
@@ -1,7 +1,10 @@
 <?php
 $link = mysqli_connect('localhost', 'root' , '','lab');
 
-mysqli_query($link, "INSERT INTO `users` (`id`, `login`, `password`, `name`, `surname`, `lang`, `role`) VALUES (NULL, '{$_POST['login']}', '{$_POST['password']}', '{$_POST['name']}', '{$_POST['surname']}', '{$_POST['lang']}', '{$_POST['role']}')");  
+$stmt = mysqli_prepare($link, "INSERT INTO `users` (`login`, `password`, `name`, `surname`, `lang`, `role`) VALUES (?, ?, ?, ?, ?, ?)");
+mysqli_stmt_bind_param($stmt, "sssssi", $_POST['login'], $_POST['password'], $_POST['name'], $_POST['surname'], $_POST['lang'], $_POST['role']);
+
+mysqli_stmt_execute($stmt);  
 
 header("Location:../list.php");
 ?>
