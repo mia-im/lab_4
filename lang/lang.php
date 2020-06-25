b584fe465b3f75cf054b10b319b65cfe4f97044b
mia-im <62039441+mia-im@users.noreply.github.com>
2020-06-25 19:28:51 +0300

    Update lang.php

--- lang/lang.php
+++ lang/lang.php
@@ -3,7 +3,10 @@ session_start();
 $link = mysqli_connect('localhost', 'root' , '','lab');
 
 $_SESSION['lang']=$_POST['lang'];
-mysqli_query($link, "UPDATE `users` SET `lang` = '{$_POST['lang']}' WHERE `id`='" . mysqli_real_escape_string($link, $_SESSION['id']) . "';");
+$stmt = mysqli_prepare($link, "UPDATE `users` SET `lang` = ? WHERE `id` = ?");
+mysqli_stmt_bind_param($stmt, "si", $_POST['lang'], $_SESSION['id']);
+mysqli_stmt_execute($stmt);
+
 header("Location:../login/if.php");
 ?>
 
