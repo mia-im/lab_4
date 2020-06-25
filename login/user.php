33cea4a1e709eea05fa12255e897024d7f7f2cf8
mia-im <62039441+mia-im@users.noreply.github.com>
2020-06-25 19:38:41 +0300

    Update user.php

--- login/user.php
+++ login/user.php
@@ -1,13 +1,13 @@
 <?php
 $link = mysqli_connect('localhost', 'root' , '','lab');
 
-$login = $_POST["login"];
-$password = $_POST["password"];
+$stmt = mysqli_prepare($link, "SELECT * FROM `users` WHERE `login` = ? AND `password` = ?");
+mysqli_stmt_bind_param($stmt, "ss", $_POST["login"], $_POST["password"]);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
 
-$result = mysqli_query($link, "SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password' ");
+$user = mysqli_fetch_assoc($result);
+
 include '../actions/dop/session.php';
-
 header("Location: if.php");
 ?>																																																
-
-
