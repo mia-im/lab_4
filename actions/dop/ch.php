1a520cd2a3fa523b3dd9d05256fcd4b8fc051e8f
mia-im <62039441+mia-im@users.noreply.github.com>
2020-06-25 19:46:18 +0300

    Update ch.php

--- actions/dop/ch.php
+++ actions/dop/ch.php
@@ -2,8 +2,6 @@
 session_start();
 $link = mysqli_connect('localhost', 'root' , '','lab');
 
-$id=(int)$_GET['id'];
-
 if ($id==$_SESSION['id']) {
 	$stmt = mysqli_prepare($link, "UPDATE `users` SET `name` = ?,`surname` = ?,`login` = ?, `password` = ?,`lang` = ? WHERE `id` = ?");
 mysqli_stmt_bind_param($stmt, "sssssi", $_POST['name'], $_POST['surname'], $_POST['login'], $_POST['password'], $_POST['lang'], $_GET['id']);
