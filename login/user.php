33525ca91b3a877b9303270587b75b6e6738df80
mia-im <62039441+mia-im@users.noreply.github.com>
2020-06-25 19:46:50 +0300

    Update user.php

--- login/user.php
+++ login/user.php
@@ -6,8 +6,6 @@ mysqli_stmt_bind_param($stmt, "ss", $_POST["login"], $_POST["password"]);
 mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
 
-$user = mysqli_fetch_assoc($result);
-
 include '../actions/dop/session.php';
 header("Location: if.php");
 ?>																																																
